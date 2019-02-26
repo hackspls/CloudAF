e647f868055292975dae2abdb4657b8468320204
hackspls <45718019+hackspls@users.noreply.github.com>
2019-02-26 12:44:54 -0800

    Update init_ans_master.sh

--- init_ans_master.sh
+++ init_ans_master.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-yum updrade -y
-yum install -y vim epel-release
-yum update -y
-yum install -y git python python-devel python-pip openssl ansible
-sed -i '22s/#//' /etc/ansible/ansible.cfg
-sed -i '14s/#//' /etc/ansible/ansible.cfg
+#!/bin/bash\n
+yum updrade -y\n
+yum install -y vim epel-release\n
+yum update -y\n
+yum install -y git python python-devel python-pip openssl ansible\n
+sed -i '22s/#//' /etc/ansible/ansible.cfg\n
+sed -i '14s/#//' /etc/ansible/ansible.cfg\n
